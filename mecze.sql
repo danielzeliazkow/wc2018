220880249420962ad06dd60b49f85fe04afed399
danielzeliazkow <37705656+danielzeliazkow@users.noreply.github.com>
2018-06-05 18:04:42 +0200

    Create mecze.sql

---
+++ mecze.sql
@@ -0,0 +1,97 @@
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('14-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Rosja', 'Arabia Saudyjska');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('15-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Egipt', 'Urugwaj');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('15-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Maroko', 'Iran');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('15-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Portugalia', 'Hiszpania');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('16-06-2018 12:00', 'DD-MM-YYYY HH24:MI'), false, 'Francja', 'Australia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('16-06-2018 15:00', 'DD-MM-YYYY HH24:MI'), false, 'Argentyna', 'Islandia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('16-06-2018 18:00', 'DD-MM-YYYY HH24:MI'), false, 'Peru', 'Dania');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('16-06-2018 21:00', 'DD-MM-YYYY HH24:MI'), false, 'Chorwacja', 'Nigeria');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('17-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Kostaryka', 'Serbia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('17-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Niemcy', 'Meksyk');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('17-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Brazylia', 'Szwajcaria');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('18-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Szwecja', 'Korea Płd.');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('18-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Belgia', 'Panama');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('18-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Tunezja', 'Anglia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('19-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Kolumbia', 'Japonia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('19-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Polska', 'Senagal');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('19-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Rosja', 'Egipt');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('20-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Portugalia', 'Maroko');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('20-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Urugwaj', 'Arabia Saudyjska');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('20-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Iran', 'Hiszpania');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('21-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Dania', 'Australia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('21-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Francja', 'Peru');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('21-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Argentyna', 'Chorwacja');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('22-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Brazylia', 'Kostaryka');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('22-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Nigeria', 'Islandia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('22-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Serbia', 'Szwajcaria');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('23-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Korea Płd', 'Meksyk');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('23-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Niemcy', 'Szwecja');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('23-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Belgia', 'Tunezja');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('24-06-2018 14:00', 'DD-MM-YYYY HH24:MI'), false, 'Anglia', 'Panama');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('24-06-2018 17:00', 'DD-MM-YYYY HH24:MI'), false, 'Japonia', 'Senagal');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('24-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Polska', 'Kolumbia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('25-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Urugwaj', 'Rosja');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('25-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Arabia Saudyjska', 'Egipt');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('25-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Hiszpania', 'Maroko');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('25-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Iran', 'Portugalia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('26-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Dania', 'Francja');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('26-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Australia', 'Peru');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('26-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Islandia', 'Chorwacja');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('26-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Nigeria', 'Argentyna');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('27-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Meksyk', 'Szwecja');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('27-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Korea Płd.', 'Niemcy');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('27-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Serbia', 'Brazylia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('27-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Szwajcaria', 'Kostaryka');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('28-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Senagal', 'Kolumbia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('28-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Japonia', 'Polska');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('28-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Anglia', 'Belgia');
+INSERT INTO public.match(id, date, isfinished, team_one_name , team_two_name)
+VALUES (nextval('matchsequence'), to_timestamp('28-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Panama', 'Tunezja');
+commit;
