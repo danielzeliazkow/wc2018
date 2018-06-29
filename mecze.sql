68554e0383b1611dc41dbe4a3a27776dccdc3d71
danielzeliazkow <37705656+danielzeliazkow@users.noreply.github.com>
2018-06-29 12:06:08 +0200

    Update mecze.sql

--- mecze.sql
+++ mecze.sql
@@ -1,16 +1,16 @@
 INSERT INTO match(id, date, isfinished, team_one_name , team_two_name)
-VALUES (nextval('matchsequence'), to_timestamp('30-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Francja', 'Argentyna');
+VALUES (101, to_timestamp('30-06-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Francja', 'Argentyna');
 INSERT INTO match(id, date, isfinished, team_one_name , team_two_name)
-VALUES (nextval('matchsequence'), to_timestamp('30-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Urugwaj', 'Portugalia');
+VALUES (102, to_timestamp('30-06-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Urugwaj', 'Portugalia');
 INSERT INTO match(id, date, isfinished, team_one_name , team_two_name)
-VALUES (nextval('matchsequence'), to_timestamp('01-07-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Hiszpania', 'Rosja');
+VALUES (103, to_timestamp('01-07-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Hiszpania', 'Rosja');
 INSERT INTO match(id, date, isfinished, team_one_name , team_two_name)
-VALUES (nextval('matchsequence'), to_timestamp('01-07-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Chorwacja', 'Dania');
+VALUES (104, to_timestamp('01-07-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Chorwacja', 'Dania');
 INSERT INTO match(id, date, isfinished, team_one_name , team_two_name)
-VALUES (nextval('matchsequence'), to_timestamp('02-07-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Brazylia', 'Meksyk');
+VALUES (105, to_timestamp('02-07-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Brazylia', 'Meksyk');
 INSERT INTO match(id, date, isfinished, team_one_name , team_two_name)
-VALUES (nextval('matchsequence'), to_timestamp('02-07-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Belgia', 'Japonia');
+VALUES (106, to_timestamp('02-07-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Belgia', 'Japonia');
 INSERT INTO match(id, date, isfinished, team_one_name , team_two_name)
-VALUES (nextval('matchsequence'), to_timestamp('03-07-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Szwecja', 'Szwajcaria');
+VALUES (107, to_timestamp('03-07-2018 16:00', 'DD-MM-YYYY HH24:MI'), false, 'Szwecja', 'Szwajcaria');
 INSERT INTO match(id, date, isfinished, team_one_name , team_two_name)
-VALUES (nextval('matchsequence'), to_timestamp('03-07-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Kolumbia', 'Anglia');
+VALUES (108, to_timestamp('03-07-2018 20:00', 'DD-MM-YYYY HH24:MI'), false, 'Kolumbia', 'Anglia');
